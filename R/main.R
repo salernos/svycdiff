2451e92129277f6523c5be97840fc9072be06918
salernos <salerno1212@gmail.com>
2025-05-20 18:48:59 -0400

    Correcting data generation function to properly calculate controlled difference with new outcome model definition. Compartmentalizing main function and creating helpers for readability and computational efficiency.

--- R/main.R
+++ R/main.R
@@ -1,20 +1,3 @@
-#===============================================================================
-#
-#  PROGRAM: main.R
-#
-#  AUTHOR:  Stephen Salerno (ssalerno@fredhutch.org)
-#
-#  PURPOSE: Implements the main function (svycdiff) for estimating the
-#           population average controlled difference (ACD) or population
-#           average treatment effect (ATE) in complex surveys where
-#           selection depends on the comparison group of interest.
-#
-#  UPDATED: 2025-04-22
-#
-#===============================================================================
-
-#=== MAIN FUNCTION =============================================================
-
 #' Controlled Difference Estimation for Complex Surveys
 #'
 #' @description
@@ -122,292 +105,59 @@
 #'
 #' @importFrom survey svydesign svyglm
 #'
-#' @importFrom stats coef glm lm model.matrix qlogis plogis pnorm predict qnorm
+#' @importFrom stats coef glm lm model.matrix qlogis plogis pnorm predict qt
 #'
 #' @export
-svycdiff <- function(df,
-                     id_form,
-                     a_form,
-                     s_form,
-                     y_form,
-                     y_fam = NULL,
-                     strata = NULL,
-                     cluster = NULL) {
 
-  #--- SETUP -------------------------------------------------------------------
+svycdiff <- function(
 
-  #-- DATA
-
-  yy <- df[, all.vars(y_form)[1]]
-  aa <- df[, all.vars(a_form)[1]]
-  ss <- df[, all.vars(s_form)[1]]
-
-  df$ss <- ss
-
-  df1 <- df; df1[[a_form[[2]]]] <- 1
-  df0 <- df; df0[[a_form[[2]]]] <- 0
-
-  S_known <- ifelse(s_form[[3]] == 1, TRUE, FALSE)
+  df,
+  id_form,
+  a_form,
+  s_form,
+  y_form,
+  y_fam = NULL,
+  strata = NULL,
+  cluster = NULL) {
 
   #-- ASSERTIONS
 
-  if (id_form == "OM" || id_form == "DR") {
+  id_form <- match.arg(id_form, c("OM", "IPW1", "IPW2", "DR"))
 
-    if (is.null(y_fam)) stop("Must supply `y_fam` for OM or DR.")
-  }
+  if (id_form %in% c("OM", "DR")) stopifnot(!is.null(y_fam))
 
-  if (y_form[[3]] == 1) {
+  #-- HELPER FUNCTIONS
 
-    fit_y <- NULL
-  }
+  prep <- prep_data(df, a_form, s_form, y_form)
 
-  if (!all(aa %in% c(0,1))) stop("`A` must be coded 0/1.")
+  models <- fit_models(prep, id_form, a_form, s_form, y_form, y_fam)
 
-  #--- FIT COMMON MODELS -------------------------------------------------------
+  ests <- compute_targets(models, prep, id_form)
 
-  #-- Pr(A = 1 | X, S = 1): Within-Sample Propensity Score
+  u_func <- make_uclosure(prep, models, id_form, y_fam)
 
-  fit_a <- glm(a_form, family = "quasibinomial", data = df)
+  vcov <- compute_vcov(u_func, ests, prep, strata, cluster)
 
-  P_A1_cond_X <- predict(fit_a, newdata = df, type = "response")
+  #-- FINAL OUTPUT
 
-  #-- Pr(A = 1 | X): Weighted Propensity Score
+  est <- ests[length(ests)]
 
-  des_wtd_a <- svydesign(ids = ~ 1, weights = 1 / df$ss, data = df)
+  se <- sqrt(vcov[nrow(vcov), ncol(vcov)])
 
-  wtd_fit_a <- svyglm(a_form, des_wtd_a, family = "quasibinomial")
+  ci <- est + c(-1, 1) * qt(0.975, df = nrow(df) - 1) * se
 
-  wtd_P_A1_cond_X <- predict(wtd_fit_a, newdata = df, type = "response")
+  pval <- 2 * (1 - pnorm(abs(est) / se))
 
-  #-- Pr(S = 1 | A, X): Probability of Selection
+  structure(
+    list(
+      id_form   = id_form,
+      cdiff     = c(est = est, err = se, lcl = ci[1], ucl = ci[2], pval = pval),
+      fit_a     = models$fit_a,
+      wtd_fit_a = models$wtd_fit_a,
+      fit_s     = models$fit_s,
+      fit_y     = models$fit_y
+    ),
 
-  if (S_known) {
-
-    fit_s <- NULL
-
-    #- Pr(S = 1 | A = 1, X): Probability of Selection Among A = 1
-
-    P_S1_cond_A1X <- df$P_S_cond_A1X
-
-    #- Pr(S = 1 | A = 0, X): Probability of Selection Among A = 0
-
-    P_S1_cond_A0X <- df$P_S_cond_A0X
-
-  } else {
-
-    fit_s <- betareg(s_form, data = df)
-
-    #- Pr(S = 1 | A = 1, X): Probability of Selection Among A = 1
-
-    P_S1_cond_A1X <- predict(fit_s, newdata = df1, type = "response")
-
-    #- Pr(S = 1 | A = 0, X): Probability of Selection Among A = 0
-
-    P_S1_cond_A0X <- predict(fit_s, newdata = df0, type = "response")
-  }
-
-  #-- Pr(S = 1 | X): Probability of Selection, Marginalized Over A
-
-  P_S1_cond_X <- P_S1_cond_A1X*wtd_P_A1_cond_X +
-
-    P_S1_cond_A0X*(1 - wtd_P_A1_cond_X)
-
-  #- Pr(S = 1): Marginal Sampling Fraction
-
-  P_S1 <- length(ss) / sum(1/ss)
-
-  #--- METHOD-SPECIFIC CALCULATIONS --------------------------------------------
-
-  if (id_form == "OM"){
-
-    fit_y <- glm(y_form, family = y_fam, data = df)
-
-    m0 <- predict(fit_y, newdata = df0, type = "response")
-
-    m1 <- predict(fit_y, newdata = df1, type = "response")
-
-    est <- mean((m1 - m0)/P_S1_cond_X)*P_S1
-
-    if (S_known) {
-
-      ests <- c(coef(wtd_fit_a), coef(fit_y), cdiff = est)
-
-      ests_dims <- c(length(coef(wtd_fit_a)), length(coef(fit_y)), 1)
-
-    } else {
-
-      ests <- c(coef(wtd_fit_a), coef(fit_s), coef(fit_y), cdiff = est)
-
-      ests_dims <- c(
-
-        length(coef(wtd_fit_a)), length(coef(fit_s)), length(coef(fit_y)), 1)
-    }
-
-  } else if (id_form == "IPW1") {
-
-    est <- mean((aa*yy/wtd_P_A1_cond_X/P_S1_cond_A1X) -
-
-      ((1 - aa)*yy/(1 - wtd_P_A1_cond_X)/P_S1_cond_A0X))*P_S1
-
-    if (S_known) {
-
-      ests <- c(coef(wtd_fit_a), cdiff = est)
-
-      ests_dims <- c(length(coef(wtd_fit_a)), 1)
-
-    } else {
-
-      ests <- c(coef(wtd_fit_a), coef(fit_s), cdiff = est)
-
-      ests_dims <- c(length(coef(wtd_fit_a)), length(coef(fit_s)), 1)
-    }
-
-  } else if (id_form == "IPW2") {
-
-    est <- mean(((aa*yy/P_A1_cond_X) - ((1 - aa)*yy/(1 - P_A1_cond_X))) /
-
-      P_S1_cond_X)*P_S1
-
-    if (S_known) {
-
-      ests <- c(coef(fit_a), coef(wtd_fit_a), cdiff = est)
-
-      ests_dims <- c(length(coef(fit_a)), length(coef(wtd_fit_a)), 1)
-
-    } else {
-
-      ests <- c(coef(fit_a), coef(wtd_fit_a), coef(fit_s), cdiff = est)
-
-      ests_dims <- c(
-
-        length(coef(fit_a)), length(coef(wtd_fit_a)), length(coef(fit_s)), 1)
-    }
-
-  } else if (id_form == "DR") {
-
-    fit_y <- glm(y_form, family = y_fam, data = df)
-
-    m1 <- predict(fit_y, newdata = df1, type = "response")
-
-    m0 <- predict(fit_y, newdata = df0, type = "response")
-
-    mu1 <- (m1 + aa * (yy - m1) / P_A1_cond_X)
-
-    mu0 <- (m0 + (1 - aa) * (yy - m0) / (1 - P_A1_cond_X))
-
-    est <- mean((mu1 - mu0)/P_S1_cond_X)*P_S1
-
-    if (S_known) {
-
-      ests <- c(coef(fit_a), coef(wtd_fit_a), coef(fit_y), cdiff = est)
-
-      ests_dims <- c(length(coef(wtd_fit_a)), length(coef(fit_y)), 1)
-
-    } else {
-
-      ests <- c(coef(fit_a), coef(wtd_fit_a), coef(fit_s),
-
-        coef(fit_y), cdiff = est)
-
-      ests_dims <- c(length(coef(fit_a)), length(coef(wtd_fit_a)),
-
-        length(coef(fit_s)), length(coef(fit_y)), 1)
-    }
-
-  } else {
-
-    stop("id_form must be one of 'OM', 'IPW1', 'IPW2', or 'DR'")
-  }
-
-  #--- INFERENCE ---------------------------------------------------------------
-
-  halfmeat <- .U(
-
-    ests, ests_dims, df, id_form, a_form, s_form, y_form, y_fam, S_known)
-
-  bread <- jacobian(.G, ests,
-
-    theta_dims = ests_dims, df = df, id_form = id_form, a_form = a_form,
-
-    s_form = s_form, y_form = y_form, y_fam = y_fam, S_known = S_known)
-
-  IF <- tcrossprod(halfmeat, solve(-bread))
-
-  if (!is.null(strata)) {
-
-    err <- matrix(0, nrow = ncol(IF), ncol = ncol(IF))
-
-    stratas <- df[, strata]
-
-    strata_levels <- unique(stratas)
-
-    if (!is.null(cluster)) {
-
-      for (k in 1:length(strata_levels)) {
-
-        df_k <- df[which(stratas == strata_levels[k]), ]
-
-        IF_k <- IF[which(stratas == strata_levels[k]), ]
-
-        clusters_k <- df_k[, cluster]
-
-        clusters_k_levels <- unique(clusters_k)
-
-        err_gk <- matrix(0, nrow = ncol(IF), ncol = ncol(IF))
-
-        tot_IF_g <- vector("list", length(clusters_k_levels))
-
-        for(g in 1:length(clusters_k_levels)) {
-
-          IF_g <- IF_k[which(clusters_k == clusters_k_levels[g]), ]
-
-          tot_IF_g[[g]] <- colSums(IF_g)
-        }
-
-        avg_IF_k <- Reduce('+', tot_IF_g) / length(clusters_k_levels)
-
-        err_gk <- Reduce('+', lapply(tot_IF_g, function(g) {
-
-          tcrossprod(g - avg_IF_k)
-
-        }))
-
-        err <- err +
-
-          (length(clusters_k_levels) / (length(clusters_k_levels) - 1))*err_gk
-      }
-
-      err <- sqrt(err[nrow(err), ncol(err)])
-
-    } else {
-
-      # NEED
-    }
-
-  } else {
-
-    err <- sqrt(sum(IF[, ncol(IF)]^2))
-
-  }
-
-  lcl <- est - qnorm(0.975)*err
-
-  ucl <- est + qnorm(0.975)*err
-
-  pval <- 2*(1 - pnorm(abs(est)/err))
-
-  out <- c(est = est, err = err, lcl = lcl, ucl = ucl, pval = pval)
-
-  return(
-
-    structure(
-
-      list(id_form = id_form, cdiff = out, fit_y = fit_y, fit_a = fit_a,
-
-        wtd_fit_a = wtd_fit_a, fit_s = fit_s),
-
-      class = "svycdiff")
+    class = "svycdiff"
   )
 }
-
-#=== END =======================================================================
